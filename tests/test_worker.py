a63e69b76214c0a4d590a95656c4f7023d784422
igboke <danieligboke669@gmail.com>
2025-12-15 08:44:48 +0100

    fix: added pytest skip, to fix error on push

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -6,10 +6,12 @@ def test_genai_response(tweet,worker):
     response = worker.analyze_tweet(tweet,["gore","rape"])
     assert "NO" in response
 
+@pytest.mark.skipif(not os.getenv("GEMINI_API_KEY"), reason="No Gemini API Key found")
 def test_worker_reason(valid_reason,worker):
     reason = worker.get_reason(valid_reason)
     assert reason == "Content does not exist. It is analyzed safe"
 
+@pytest.mark.skipif(not os.getenv("GEMINI_API_KEY"), reason="No Gemini API Key found")
 def test_invalid_reason(invalid_reason,worker):
     with pytest.raises(Exception) as e:
         reason = worker.get_reason(invalid_reason)
